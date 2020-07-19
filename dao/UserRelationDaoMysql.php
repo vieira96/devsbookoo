240fcf8987a92ca11eabe9161a2dd0823c714599
samuel vieira <samuelvieira@oi.com.br>
2020-07-19 09:32:26 -0300

    funçao de seguir adicionada

--- dao/UserRelationDaoMysql.php
+++ dao/UserRelationDaoMysql.php
@@ -2,6 +2,7 @@
 
 require_once 'models/UserRelation.php';
 
+
 class UserRelationDaoMysql implements UserRelationDAO {
 
     private $pdo;
@@ -13,7 +14,38 @@ class UserRelationDaoMysql implements UserRelationDAO {
 
     public function insert(UserRelation $userRelation)
     {
+
+    }
+
+    public function followToggle(UserRelation $userRelation)
+    {
+        $me = $userRelation->user_from;
+        $other = $userRelation->user_to;
+
+        if($this->isFollowing($me, $other)) {
+            $sql = $this->pdo->prepare("DELETE FROM userrelations WHERE user_from = :user_from AND user_to = :user_to");
+        } else {
+            $sql = $this->pdo->prepare("INSERT INTO userrelations (user_from, user_to) VALUES (:user_from, :user_to)");
+        }
         
+        $sql->bindValue(":user_from", $me);
+        $sql->bindValue(":user_to", $other);
+        $sql->execute();
+
+    }
+
+    public function isFollowing($me, $other)
+    {
+        $sql = $this->pdo->prepare("SELECT * FROM userrelations WHERE user_from = :user_from AND user_to = :user_to");
+        $sql->bindValue(":user_from", $me);
+        $sql->bindValue(":user_to", $other);
+        $sql->execute();
+
+        if($sql->rowCount() > 0) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public function getfollowings($id)
--- profile.php
+++ profile.php
@@ -4,10 +4,14 @@ require_once 'config.php';
 require_once 'models/Auth.php';
 require_once 'dao/PostDaoMysql.php';
 require_once 'dao/UserDaoMysql.php';
+require_once 'dao/UserRelationDaoMysql.php';
 
 $activeMenu = '';
 $auth = new Auth($pdo, $base);
 $userInfo = $auth->checkToken();
+$urDao = new UserRelationDaoMysql($pdo);
+
+
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRIPPED);
 
 if(!$id){
@@ -39,7 +43,7 @@ $user->ageYears = $dateFrom->diff($dateTo)->y; // pega a diferença entre os ano
 $feed = $postDao->getUserFeed($id);
 
 //verificar se sigo esse usuario
-
+$isFollowing = $urDao->isFollowing($userInfo->id, $id);
 
 require 'partials/header.php';
 require 'partials/menu.php'; 
@@ -47,7 +51,44 @@ require 'partials/menu.php';
 
 <section class="feed">
 
-<?php require 'partials/feed-header.php'; ?>
+<div class="row">
+    <div class="box flex-1 border-top-flat">
+        <div class="box-body">
+            <div class="profile-cover" style="background-image: url('<?=$base;?>/media/covers/<?=$user->cover;?>');"></div>
+            <div class="profile-info m-20 row">
+                <div class="profile-info-avatar">
+                    <img src="<?=$base?>/media/avatars/<?=$user->avatar;?>" />
+                </div>
+                <div class="profile-info-name">
+                    <div class="profile-info-name-text"><?=$user->name;?></div>
+                    <?php if(!empty($user->city)): ?>
+                        <div class="profile-info-location"><?=$user->city;?></div>
+                    <?php endif; ?>
+                </div>
+                <div class="profile-info-data row">
+                
+                    <?php if($id != $userInfo->id): ?>
+                        <div class="profile-info-item m-width-20">
+                            <a class="button" href="<?=$base;?>/follow_action.php?id=<?=$id;?>"> <?php if($isFollowing): ?> Deixar de Seguir <?php else: ?> Seguir <?php endif; ?></a>
+                        </div>
+                    <?php endif; ?>
+                    <div class="profile-info-item m-width-20">
+                        <div class="profile-info-item-n"><?=count($user->followers)?></div>
+                        <div class="profile-info-item-s">Seguidores</div>
+                    </div>
+                    <div class="profile-info-item m-width-20">
+                        <div class="profile-info-item-n"><?=count($user->followings);?></div>
+                        <div class="profile-info-item-s">Seguindo</div>
+                    </div>
+                    <div class="profile-info-item m-width-20">
+                        <div class="profile-info-item-n"><?=count($user->photos);?></div>
+                        <div class="profile-info-item-s">Fotos</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="row">
 
